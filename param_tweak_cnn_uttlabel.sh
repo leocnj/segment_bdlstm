84505d099e8521b1a81e6b87252c594d1076206d
Chen, Lei <LChen@ETS.ORG>
2016-12-29 10:55:45 -0500

    need use updated exp_name

--- param_tweak_cnn_uttlabel.sh
+++ param_tweak_cnn_uttlabel.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 KERAS_BACKEND=theano python app/train_pair_uttlabel.py \
- --exp_name=cnn\
+ --exp_name=uttlabel_cnn\
  --data_dir=csv/ \
  --embedding_file_path=embd/glove.6B.300d.txt \
  --embedding_dim=300 \
