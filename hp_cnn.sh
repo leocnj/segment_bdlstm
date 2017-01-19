13c8822da129c510672a049b60b9691e2f7a7874
Chen, Lei <LChen@ETS.ORG>
2017-01-19 10:17:07 -0500

    Updated hp_cnn.sh
    - $1 to pass GPU #
    - added max_evals

--- hp_cnn.sh
+++ hp_cnn.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-KERAS_BACKEND=theano python app/tweak_params.py \
+echo 'GPU:' $1
+KERAS_BACKEND=theano THEANO_FLAGS='device=${1}' python app/tweak_params.py \
  --data_dir=csv/ \
  --exp_name=cnn \
  --embedding_file_path=embd/glove.6B.300d.txt \
  --embedding_dim=300 \
  --nb_words=5300 \
  --max_sequence_len=40 \
- --num_epochs=30
+ --num_epochs=30 \
+ --max_evals=100
