3db7a8269760cd927d0cbe440a55c321324e36a8
Chen, Lei <LChen@ETS.ORG>
2017-01-18 23:14:58 -0500

    loss (now corr) should not be negative

--- app/tweak_params.py
+++ app/tweak_params.py
@@ -86,7 +86,7 @@ def model_to_tweak(params):
 
     corr_r = pearsonr(y_test, pred)
     print('Test Pearson corr: {}.'.format(corr_r))
-    return {'loss': -1.0 * float(corr_r[0]), 'status': STATUS_OK, 'model': model}
+    return {'loss': max(-1.0*float(corr_r[0]), 0), 'status': STATUS_OK, 'model': model}
 
 
 if __name__ == '__main__':
