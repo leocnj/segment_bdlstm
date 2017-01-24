dc7912289c8fdf6856ab32d50b9e735f0be59d5d
Chen, Lei <LChen@ETS.ORG>
2017-01-24 10:50:48 -0500

    Will retune segment CNN.

--- app/tweak_params.py
+++ app/tweak_params.py
@@ -139,8 +139,8 @@ if __name__ == '__main__':
                  'batch_size': hp.choice('batch_size', [3, 6, 9]),
                  'filter_size': hp.choice('filter_size', [3, 4, 5, 6]),
                  'nb_filter': hp.choice('nb_filter', [50, 75, 100]),
-                 'dropout1': 0.5,
-                 'dropout2': 0.5,
+                 'dropout1': hp.uniform('dropout1', 0.25, 0.75),
+                 'dropout2': hp.uniform('dropout2', 0.25, 0.75),
                  'embeddings_trainable': False}
         trials = Trials()
         best = fmin(model_to_tweak, space, algo=tpe.suggest, max_evals=args.max_evals, trials=trials)
